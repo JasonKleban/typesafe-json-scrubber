469ee0571165daef984231a913c213e6843ceac8
Jason Kleban <jkleban@outlook.com>
2018-09-26 19:20:00 -0400

    upgrades, es5 output

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -10,16 +10,16 @@ export interface ScrubLog {
 
 export interface Scrubber<T, P> {
     /** scrub the obj */
-    scrub(obj : any) : { scrubbed : P | undefined, scrubLog : ScrubLog[] }
+    scrub(obj : unknown) : { scrubbed : P | undefined, scrubLog : ScrubLog[] }
 }
 
 export interface ObjectScrubber<T, P = {}> extends Scrubber<T, P> {
     /** memberName : string */
     must<M extends Extract<keyof T, string>, S>(memberName : M, type : new (path : string) => Scrubber<T[M], S>) : ObjectScrubber<T, P & Record<M, S>>
-    
+
     /** memberName : string = defaultValue */
     should<M extends Extract<keyof T, string>, S>(memberName : M, type : new (path : string) => Scrubber<T[M], S>, defaultValue : T[M]) : ObjectScrubber<T, P & Record<M, S>>
-    
+
     /** memberName? : string */
     may<M extends Extract<keyof T, string>, S>(memberName : M, type : new (path : string) => Scrubber<T[M], S>) : ObjectScrubber<T, P & PartialRecord<M, S>>
 }
